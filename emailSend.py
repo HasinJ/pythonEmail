161bb96fbd397364721da9a99a8dd4b5c1a0d977
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-11-21 17:43:26 -0500

    sends email

--- emailSend.py
+++ emailSend.py
@@ -1,11 +1,17 @@
 import smtplib, ssl, getpass
 
 port = 465  # For SSL
-password = getpass.getpass("Type your password and press enter: ")
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = f"""\
+Subject: Error in Application
 
-# Create a secure SSL context
+Application has thrown an error for today. Please check logs for "date".\n
+DO NOT REPLY."""
+
 context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("hasin.choudhury@cplusmanagement.com", password)
-    # TODO: Send email here
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
